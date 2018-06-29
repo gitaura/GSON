0fd106b11662953e8d1f413b8a13082f83d90e11
Aurelia <aurela87@o2.pl>
2018-06-29 18:44:07 +0200

    first commit

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,14 +5,11 @@ public class Person {
     int height;
     String hobby;
 
-
-
     public Person(String name, String surname, int age, int height, String hobby, Person person) {
         this.name = "Jan";
         this.surname = "Kowalski";
         this.age = 50;
         this.height = 180;
         this.hobby = "joga";
-
     }
 }
