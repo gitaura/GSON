c7bd33377f1ef1fc0c85951096566bdf0c41b8a0
Aurelia <aurela87@o2.pl>
2018-06-29 19:31:47 +0200

    modified PersonClass and Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,15 @@ import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
 
-        Gson gson = new Gson();
-    }
+        Person person = new Person("Jan", "Kowalski", 50,180,"joga");
 
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+
+        Person person2 = gson.fromJson(json,Person.class);
+        System.out.println(person2);
+    }
 }
 
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,11 +5,22 @@ public class Person {
     int height;
     String hobby;
 
-    public Person(String name, String surname, int age, int height, String hobby, Person person) {
-        this.name = "Jan";
-        this.surname = "Kowalski";
-        this.age = 50;
-        this.height = 180;
-        this.hobby = "joga";
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+
+    public Person(String name, String surname, int age, int height, String hobby) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.height = height;
+        this.hobby = hobby;
     }
 }
